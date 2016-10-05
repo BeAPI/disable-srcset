f2e5a0b08ec0323826d62f59f311222eddb52f2d
Nicolas Kulka <kulka.nicolas@gmail.com>
2016-10-05 15:52:28 +0200

    Update disable-srcset.php

--- disable-srcset.php
+++ disable-srcset.php
@@ -1,15 +1,31 @@
 <?php
 /*
  Plugin Name: BEA Disable SRCSet
+ Version: 1.0.1
  Plugin URI: http://www.beapi.fr
  Description: Remove on the site responsive images
- Author: BeAPI
+ Author: BE API Technical team
  Author URI: http://www.beapi.fr
- Version: 1.0.1
-
+ Domain Path: languages
+ Text Domain: disable-srcset
+ 
  ----
- Copyright 2016 BE API Technical Team (technique@beapi.fr)
- ----
+ 
+ Copyright 2016 BE API Technical team (human@beapi.fr)
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 // Clean the up the image from wp_get_attachment_image()
